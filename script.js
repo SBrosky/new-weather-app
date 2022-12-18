039db392b74f64ad925196da5f3ce134ac97fa9d
SBrosky <steph.brosky@gmail.com>
2022-12-17 20:04:12 -0800

    added call to One Call API to JS script

--- script.js
+++ script.js
@@ -29,6 +29,8 @@ function updateTemp(response) {
       "alt",
       `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
     );
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -73,7 +75,8 @@ function getCurrent() {
   navigator.geolocation.getCurrentPosition(getPlace);
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElemnet = document.querySelector("#forecast");
   let forecastHTML = `<div class="row forecast-days">`;
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -89,6 +92,15 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElemnet.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = `b95f179627c8dd37f41e1be6e3250e19`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric
+`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 let days = [
@@ -130,4 +142,3 @@ currentButton.addEventListener("click", getCurrent);
 let celsuisTemp = null;
 
 searchCity("Vancouver");
-displayForecast();
